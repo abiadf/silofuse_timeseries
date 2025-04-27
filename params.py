a78b6d9c631ecd65b335845c5b674e5ec5325320
abiadf <abiadfm@gmail.com>
2025-04-27 02:48:33 +0200

    Initial commit

---
+++ params.py
@@ -0,0 +1,8 @@
+"""Module for storing parameters"""
+
+# General running
+which_scenario = "Energy" # Options: [ETTh, Energy, Stock, fMRI, Mujoco]
+iterations     = 5 # num of iterations to assess the mean/std of score
+
+num_clients    = 3
+num_epochs     = 0
